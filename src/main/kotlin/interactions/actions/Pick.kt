3df7c0f87c78bfed2256945468c033b2cde660f7
broccolai <josh@magmo.co.uk>
2020-02-08 02:15:57 +0000

    Update pick command to new permissions, add cli perm check

--- src/main/kotlin/interactions/actions/Pick.kt
+++ src/main/kotlin/interactions/actions/Pick.kt
@@ -4,9 +4,10 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import ticket.Ticket
 import ticket.TicketStatus
+import utility.message
 import utility.staff
 
-object Pick : Action("pick", "tickets.staff", true) {
+object Pick : Action("pick", "tickets.staff.pick", true) {
     private fun main(commandSender: CommandSender, ticket: Ticket) {
         val uuid = if (commandSender is Player) {
             ticket.setStatus(TicketStatus.PICKED, commandSender.uniqueId)
@@ -21,11 +22,17 @@ object Pick : Action("pick", "tickets.staff", true) {
         staff(commandSender.name + " has picked " + ticket.holdersName() + "'s ticket")
     }
 
-    fun gui(player: Player, ticket: Ticket) = main(player, ticket)
+    fun gui(player: Player, ticket: Ticket) {
+        if (hasPerms(player))
+            main(player, ticket)
+    }
 
     override fun cli(commandSender: CommandSender, input: String) {
-        val ticket = retrieveTicket(commandSender, input) ?: return
+        val ticket = retrieveTicket(commandSender, input)
 
-        main(commandSender, ticket)
+        if (ticket != null)
+            main(commandSender, ticket)
+        else
+            commandSender.message("Ticket not found.")
     }
 }
