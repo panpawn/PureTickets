4d4f2f2949b7aa87b55eca5d8b846e9cb6dcec47
Josh Taylor <josh@magmo.co.uk>
2020-02-12 14:01:21 +0000

    Complete done command

--- src/main/kotlin/interactions/actions/Done.kt
+++ src/main/kotlin/interactions/actions/Done.kt
@@ -1,9 +1,24 @@
 package interactions.actions
 
+import Tickets.Companion.InventoryManager
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import ticket.Ticket
+import ticket.TicketStatus
+import utility.message
 
-object Done : Action("done", "tickets.user.done",  true) {
+object Done : Action("done", "tickets.staff.done",  true) {
+    fun gui(player: Player, ticket: Ticket) {
+        if (!hasPerms(player)) return
+
+        ticket.setStatus(TicketStatus.CLOSED)
+        InventoryManager.refresh(player)
+    }
+
     override fun cli(commandSender: CommandSender, input: String) {
+        val ticket = parseInput(commandSender, input) ?: return
 
+        ticket.setStatus(TicketStatus.CLOSED)
+        commandSender.message("Ticket closed")
     }
 }
