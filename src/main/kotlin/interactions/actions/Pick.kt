f19495b1a8b3d8e7df0ccfb87e0d2a2c5135087b
Josh Taylor <josh@magmo.co.uk>
2020-02-12 14:00:57 +0000

    Add perm check and simplify cli function

--- src/main/kotlin/interactions/actions/Pick.kt
+++ src/main/kotlin/interactions/actions/Pick.kt
@@ -3,36 +3,25 @@ package interactions.actions
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import ticket.Ticket
-import ticket.TicketStatus
-import utility.message
 import utility.staff
 
 object Pick : Action("pick", "tickets.staff.pick", true) {
     private fun main(commandSender: CommandSender, ticket: Ticket) {
-        val uuid = if (commandSender is Player) {
-            ticket.setStatus(TicketStatus.PICKED, commandSender.uniqueId)
-            commandSender.uniqueId
-        } else {
-            ticket.setStatus(TicketStatus.PICKED)
-            null
-        }
-
-        ticket.addMessage(uuid, "Picked")
+        ticket.addMessage(commandSender, "Picked")
         ticket.notify(commandSender.name + " has picked your ticket")
+
         staff(commandSender.name + " has picked " + ticket.holdersName() + "'s ticket")
     }
 
     fun gui(player: Player, ticket: Ticket) {
-        if (hasPerms(player))
-            main(player, ticket)
+        if (!hasPerms(player)) return
+
+        main(player, ticket)
     }
 
     override fun cli(commandSender: CommandSender, input: String) {
-        val ticket = retrieveTicket(commandSender, input)
+        val ticket = parseInput(commandSender, input) ?: return
 
-        if (ticket != null)
-            main(commandSender, ticket)
-        else
-            commandSender.message("Ticket not found.")
+        main(commandSender, ticket)
     }
 }
