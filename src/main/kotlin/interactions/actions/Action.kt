835bcbbbbb7309e1ee45e001c8095a3e2c82e910
Josh Taylor <josh@magmo.co.uk>
2020-02-12 13:56:04 +0000

    Redesign some of the Action class' functions

--- src/main/kotlin/interactions/actions/Action.kt
+++ src/main/kotlin/interactions/actions/Action.kt
@@ -2,12 +2,10 @@ package interactions.actions
 
 import Tickets.Companion.ChatInput
 import Tickets.Companion.TicketManager
-import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import ticket.Ticket
+import utility.getOfflinePlayer
 import utility.message
-import java.util.*
 
 abstract class Action(override val name: String, override val permission: String, override val targets: Boolean) : Command {
     abstract fun cli(commandSender: CommandSender, input: String)
@@ -19,23 +17,8 @@ abstract class Action(override val name: String, override val permission: String
         player.closeInventory()
     }
 
-    fun target(message: String) = if (message.isBlank()) null else Bukkit.getOfflinePlayer(message.split(" ")[0])
-
-    fun ticket(uuid: UUID, input: String) = TicketManager[uuid, input.split(" ").last().toIntOrNull() ?: 1]
-
-    fun retrieveTicket(commandSender: CommandSender, message: String): Ticket? {
-        println(">" + message.split(" ")[0] + "<")
-        val target = target(message)
-
-        return if (target == null) {
-            commandSender.message("That player does not exist")
-            null
-        } else {
-            val ticket = ticket(target.uniqueId, message)
-            if (ticket == null) commandSender.message("Ticket not found")
-
-            ticket
-        }
+    fun parseInput(commandSender: CommandSender, message: String) = TicketManager[getOfflinePlayer(message.split(" ").getOrNull(0))?.uniqueId, message.split(" ").last().toIntOrNull() ?: 1].also {
+        if (it == null) commandSender.message("Ticket not found")
     }
 
     fun hasPerms(player: Player) = if (player.hasPermission(permission)) true else {
