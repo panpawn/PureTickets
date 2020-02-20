cf0e5961033090ec7ac2170515d2ab32305b9bd8
Josh Taylor <josh@magmo.co.uk>
2020-02-20 21:22:53 +0000

    Create display error function in inv manager

--- src/main/kotlin/managers/InventoryManager.kt
+++ src/main/kotlin/managers/InventoryManager.kt
@@ -1,6 +1,9 @@
 package managers
 
+import Tickets.Companion.TICKETS
 import interactions.Menu
+import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
@@ -8,6 +11,8 @@ import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.scheduler.BukkitTask
+import utility.Item
+import utility.splitToAdd
 import java.util.*
 import kotlin.collections.HashMap
 
@@ -30,6 +35,17 @@ class InventoryManager : Listener {
 
     operator fun set(player: Player, task: BukkitTask) {
         pendingTasks[player.uniqueId] = task
+    }
+
+    fun displayError(location: Int, player: Player, error: String?) {
+        player.openInventory.setItem(location, Item(Material.BARRIER).apply {
+            name = "§4§lDENIED"
+            error?.splitToAdd()?.forEach { addLore(it) }
+        }.get())
+
+        pendingTasks[player.uniqueId] = Bukkit.getScheduler().runTaskLater(TICKETS, Runnable {
+            currentMenu[player.uniqueId]?.show()
+        }, 120)
     }
 
     fun refresh(player: Player) {
