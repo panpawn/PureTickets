9878623fd10c063cadb4d0a9020f3ac1fa8228e6
Josh Taylor <josh@magmo.co.uk>
2020-02-20 21:22:16 +0000

    Move splitToAdd to extensions folder

--- src/main/kotlin/utility/Extensions.kt
+++ src/main/kotlin/utility/Extensions.kt
@@ -13,3 +13,24 @@ fun staff(message: String) = Bukkit.broadcast("$prefix$message", "tickets.staff"
 fun log(message: String) = Bukkit.getConsoleSender().sendMessage("$prefix$message")
 
 fun getOfflinePlayer(input: String?) = if (input != null) Bukkit.getOfflinePlayer(input) else null
+
+fun String.splitToAdd(): List<String> {
+    var current = 0
+    val builder = StringBuilder()
+    val split = split(" ")
+
+    return sequence {
+        for (item in split) {
+            current += item.count()
+            builder.append("$item ")
+
+            if(current >= 20) {
+                yield(builder.toString())
+                builder.clear()
+                current = 0
+            }
+        }
+
+        if (builder.isNotEmpty()) yield(builder.toString())
+    }.toList()
+}
