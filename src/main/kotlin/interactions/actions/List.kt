6c8152c5cbe458233e6eb90d794fa56a2a031fbe
Josh Taylor <josh@magmo.co.uk>
2020-02-26 04:32:43 +0000

    Create list action

---
+++ src/main/kotlin/interactions/actions/List.kt
@@ -0,0 +1,36 @@
+package interactions.actions
+
+import Tickets.Companion.TicketManager
+import interactions.Action
+import interactions.menus.ViewTickets
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import ticket.Ticket
+import utility.message
+
+object List : Action("list", "tickets.staff.list", false) {
+    override fun menu(player: Player, ticket: Ticket?) {
+        player.message("Enter the name of the player you want to view")
+        getInput(player)
+    }
+
+    override fun cli(commandSender: CommandSender, input: String) {
+        TicketManager.all().forEach {
+            commandSender.message(it.currentMessage(), false)
+        }
+    }
+
+    override fun response(player: Player, message: String) {
+        val op = Bukkit.getOfflinePlayer(message)
+
+        if (!TicketManager.contains(op)) {
+            player.message("Cannot find active tickets for $message")
+            return
+        }
+
+        ViewTickets(player) {
+            TicketManager[op.uniqueId]
+        }.show()
+    }
+}
