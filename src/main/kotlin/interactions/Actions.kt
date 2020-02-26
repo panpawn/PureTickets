34e8a8686db5302a63c17abcd5a0343e12c6b98d
Josh Taylor <josh@magmo.co.uk>
2020-02-26 04:36:15 +0000

    Delete locale manager

--- src/main/kotlin/interactions/Actions.kt
+++ src/main/kotlin/interactions/Actions.kt
@@ -1,13 +1,17 @@
 package interactions
 
-import interactions.actions.Create
-import interactions.actions.Pick
-import interactions.actions.Update
+import interactions.actions.*
+import interactions.actions.List
 
 enum class Actions(val action: Action, val isAlias: Boolean = false) {
     CREATE(Create), C(Create, true),
+    CLOSE(Close), CL(Close, true),
+    DONE(Done), D(Done, true),
+    LIST(List), L(List, true),
+    REOPEN(Reopen), R(Reopen, true),
     UPDATE(Update), U(Update, true),
-    PICK(Pick), P(Pick, true);
+    PICK(Pick), P(Pick, true),
+    YIELD(Yield), Y(Yield, true);
 
     companion object {
         fun find(input: String): Actions? {
